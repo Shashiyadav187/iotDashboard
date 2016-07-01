012bb02df39829fec6d4651d218e67c57ea83758
Gordon McLellan <gordonthree@gmail.com>
2016-07-01 17:40:04 -0400

    modified:   .node-red/.config.json 	modified:   .node-red/.flows.json.backup 	modified:   .node-red/.flows_cred.json.backup 	modified:   .node-red/flows.json 	modified:   .node-red/flows_cred.json 	new file:   app.js 	new file:   bin/www 	modified:   index.js 	modified:   package.json 	new file:   public/stylesheets/style.css 	new file:   routes/index.js 	new file:   routes/users.js 	new file:   views/error.jade 	new file:   views/index.jade 	new file:   views/layout.jade

---
+++ app.js
@@ -0,0 +1,103 @@
+const port = 3000;
+var express = require('express');
+var app = express();
+
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+
+var mysql     =    require('mysql');
+var pool      =    mysql.createPool({
+    connectionLimit : 100, //important
+    host     : 'localhost',
+    user     : 'pi',
+    password : 'mypi',
+    database : 'pidb',
+    debug    :  false
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+function handle_database(req,res) {
+    
+    pool.getConnection(function(err,connection){
+        if (err) {
+          connection.release();
+          res.json({"code" : 100, "status" : "Error in connection database"});
+          return;
+        }   
+
+        console.log('connected as id ' + connection.threadId);
+        
+        connection.query("select nodename,unix_timestamp(lastseen) as 'lastseen' from nodes",function(err,rows){
+            connection.release();
+            if(!err) {
+                res.json(rows);
+            }           
+        });
+
+        connection.on('error', function(err) {      
+              res.json({"code" : 100, "status" : "Error in connection database"});
+              return;     
+        });
+  });
+}
+
+app.get("/",function(req,res){-
+          // res.send('Hello World!');
+          handle_database(req,res);
+});
+
+app.listen(port);
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,18 +1,44 @@
-// content of index.js
-const http = require('http')  
-const port = 3000
+var express   =    require("express");
+var mysql     =    require('mysql');
+var app       =    express();
+const port = 3000;
+var pool      =    mysql.createPool({
+    connectionLimit : 100, //important
+    host     : 'localhost',
+    user     : 'pi',
+    password : 'mypi',
+    database : 'pidb',
+    debug    :  false
+});
 
-const requestHandler = (request, response) => {  
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
+function handle_database(req,res) {
+    
+    pool.getConnection(function(err,connection){
+        if (err) {
+          connection.release();
+          res.json({"code" : 100, "status" : "Error in connection database"});
+          return;
+        }   
+
+        console.log('connected as id ' + connection.threadId);
+        
+        connection.query("select nodename,unix_timestamp(lastseen) as 'lastseen' from nodes",function(err,rows){
+            connection.release();
+            if(!err) {
+                res.json(rows);
+            }           
+        });
+
+        connection.on('error', function(err) {      
+              res.json({"code" : 100, "status" : "Error in connection database"});
+              return;     
+        });
+  });
 }
 
-const server = http.createServer(requestHandler)
+app.get("/",function(req,res){-
+          res.send('Hello World!');
+          handle_database(req,res);
+});
 
-server.listen(port, (err) => {  
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
-})
+app.listen(port);
